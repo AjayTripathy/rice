967e2f8548f747d14d58845c99e492aa7e28ada2
Ajay Tripathy <aurotripathy@auro-tripathys-MacBook-Pro.local>
2012-07-21 23:45:03 -0700

    server works

--- web/server.js
+++ web/server.js
@@ -5,6 +5,8 @@ var db = mongo.db('localhost:27017/rice');
 
 var patientInfo = db.collection('patientInfo');
 
+var nodemailer = require('nodemailer');
+var rest = require('restler');
 
 
 var app =  express.createServer();
@@ -17,22 +19,44 @@ app.set('views', __dirname + '/views');
 
 
 
-app.get('/emergencyRead', function(req, res){
-     var manufacturerId = req.query.manufacturerId;
-     var authToken = req.query.authToken;
+app.post('/emergencyRead', function(req, res){
+     var manufacturerId = req.body.manufacturerId;
+     var authToken = req.body.authToken;
+     var place = req.body.place;
+     //var myNumber = req.query.myNumber; 
+     var myNumber = "15102322453"
+     console.log(manufacturerId);
      if ( isValidToken(manufacturerId, authToken) ) {
-        patientInfo.findById(userId , function (err, doc) {
+        patientInfo.findOne({'manufacturerId' : manufacturerId} , function (err, doc) {
             if (err) {
               res.send({'status' : err , data : null});
               throw err;
             }
             res.send({'status' : 'ok' , data : doc});
+            var contacts = doc.contacts;
+            if (contacts){
+              for (var i = 0 , ii = contacts.length ; i < ii ; i = i + 1){
+                  contactInfo = contacts[i];
+                  var phoneNumber = contactInfo.phoneNumber;
+                  var name = contactInfo.name;
+                  if (! place){
+                    var message = doc.username + " has been injured. You are their emergency contact. They are being taken care of, and you will be contacted with more details as they become available";
+                  }
+                  else{
+                    var message = doc.username + " has been injured at " + place + ".You are their emergency contact. They are being taken care of, and you will be contacted with more details.";
+                  }
+                  sendSms(message, phoneNumber)
+              }
+            }
         });
      }
      else {
         res.send({'status' : 'permission denied' , data : null});
      }
 });
+
+
+
 
 app.post('/addInfo' , function(req, res){
     var userId = req.body.user; 
@@ -47,18 +71,26 @@ app.post('/addInfo' , function(req, res){
     }
 });
 
-app.get('/getTagInfo' , function (req, res){
-    var userName = req.query.userName;
-    var password = req.query.password;
-    var manufacturerId = req.query.manufacturerId;
-    if (isWebsiteAuthenticated(userName, password)){
-      patientInfo.findOne({'userName' : userName} , function(err , doc){
+app.post('/getTagInfo' , function (req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    var manufacturerId = req.body.manufacturerId;
+    console.log(1 , manufacturerId);
+    if (isWebsiteAuthenticated(username, password)){
+      patientInfo.findOne({'username' : username} , function(err , doc){
         if (err){
           throw err;
         }
         else{
           var token = generateAuthToken(manufacturerId);
           doc.authToken = token;
+          doc.manufacturerId = manufacturerId;
+          var id = doc['_id'].toString();
+          patientInfo.updateById(id , doc , function (err, doc2){
+            if (err){
+              throw err;
+            }
+          });
           res.send({'status' : 'ok' , 'data' : doc}); 
         }
       });
@@ -68,10 +100,14 @@ app.get('/getTagInfo' , function (req, res){
     }
 });
 
-app.get('/testGet' , function(req, res){
+app.post('/testGet' , function(req, res){
   console.log('hi');
-  res.send({'status' : 'ok'});
+  var userName = req.body.userName;
+  var password = req.body.password;
+  var manufacturerId = req.body.manufacturerId;
+  res.send({'status' : 'ok', 'data' : { "userName": userName , "password": password , "bloodType": "A+", "authToken" : 1 }} );
 })
+
 
 app.post('/signup' , function(req, res){
   var data = req.body;
@@ -90,6 +126,15 @@ var generateAuthToken = function(manufacturerId){
   return 1;
 }
 
+var sendSms = function (message, to){
+  var gvmaxUrl = 'https://www.gvmax.com/api/send';
+  var params = {'pin': '82af7ad1f0814408a49b35ac43e0c3a0' , 'number': to , 'text': message};
+  if (to !== undefined && to !== null){
+    rest.post(gvmaxUrl, {'data': params}).on('complete' , function(data){
+      console.log(data)
+    }); 
+  }   
+}
 
 app.listen(8080);
 
